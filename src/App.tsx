af09edca3d0e1ecd3ddff31ec376fe8666304a2c
Kasper <kasper.gaworski@wp.pl>
2022-10-20 18:18:06 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div>App</div>;
+import { Router, Route } from "wouter";
+import Home from "@/pages/index";
+import Translator from "@/pages/translator";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/translator" component={Translator} />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/translator.tsx
@@ -0,0 +1,5 @@
+const Home: React.FC = () => {
+  return <div>translator</div>;
+};
+
+export default Home;
